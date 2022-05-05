ea43275306bb136096a3b5e37fbf0fbf414a0491
EC2 Default User <ec2-user@ip-172-31-34-244.us-west-2.compute.internal>
2022-05-05 06:51:20 +0000

    database setting2

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', ''),
+            'host' => env('DB_HOST', 'mysql623.db.sakura.ne.jp'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', ''),
             'username' => env('DB_USERNAME', ''),
